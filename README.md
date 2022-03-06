3970abe4724c941612081b25bf6c2e34e957c0f4
sb-haz <sb_haz@icloud.com>
2022-03-06 03:26:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FiveMinuteCode.com
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  const youtube = "https://www.youtube.com/channel/UCcIywoxb-SoP1m0zrc7qV7A";
+  const twitter = "https://twitter.com/fiveminutecodes";
+  const github = "https://github.com/fiveminutecode";
+
+  return (
+    <div className="content">
+      <div className="text-box">
+        <h1>🧑‍💻 Five Minute Code</h1>
+        <div className="links">
+          <a href={youtube}>YouTube</a>
+          <p>•</p>
+          <a href={twitter}>Twitter</a>
+          <p>•</p>
+          <a href={github}>GitHub</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
